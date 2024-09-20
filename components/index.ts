28aba79ce216f823b6b1a4a7a04ee275cacedba8
Will Gomes <w1llmarcio.dev@hotmail.com>
2024-09-20 09:29:00 -0300

    feat(home-screen): add exports file to components folder

---
+++ components/index.ts
@@ -0,0 +1,7 @@
+export * from "./AppProvider";
+export * from "./ThemedText";
+export * from "./ThemedView";
+export * from "./CircleAvatar";
+export * from "./UserTile";
+export * from "./ScreenContainer";
+export * from "./Spacer";
