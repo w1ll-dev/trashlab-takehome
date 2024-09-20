02f4c59e8aa1a46669f202ed81835fb328428b74
Will Gomes <w1llmarcio.dev@hotmail.com>
2024-09-20 09:25:58 -0300

    feat(home-screen): here we add our circle avatar component
    This component will be used to render user profile image for different situations.

---
+++ components/CircleAvatar/utis.ts
@@ -0,0 +1,6 @@
+const blurhash =
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
+
+const CIRCLE_AVATAR_SIZE = 60;
+
+export { blurhash, CIRCLE_AVATAR_SIZE };
