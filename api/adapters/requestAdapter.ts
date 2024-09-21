362dd0a46dcedcef2ba4f335447489d5234286ea
Will Gomes <w1llmarcio.dev@hotmail.com>
2024-09-21 17:23:51 -0300

    feat(home-header): add queryConstraints to our requestAdapter

--- api/adapters/requestAdapter.ts
+++ api/adapters/requestAdapter.ts
@@ -1,11 +1,12 @@
 import { FirebaseCollectionRef, FirebaseSnapshot } from "@/types/firebase";
-import { getDocs, query } from "firebase/firestore";
+import { getDocs, query, QueryConstraint } from "firebase/firestore";
 
 export const requestAdapter = async <T>(
   collectionRef: FirebaseCollectionRef,
   mapperMethod: (querySnapshot: FirebaseSnapshot) => T,
+  ...queryConstraints: QueryConstraint[]
 ): Promise<T> => {
-  const collectionQuery = query(collectionRef);
+  const collectionQuery = query(collectionRef, ...queryConstraints);
 
   const querySnapshot = await getDocs(collectionQuery);
 
