d3c5795da9fdf354e1fc34747f71d511428ab356
Will Gomes <w1llmarcio.dev@hotmail.com>
2024-09-21 17:12:35 -0300

    chore(project-setup): add bulkUsers file
    The methods inside this file will add mock users data to our firebase

---
+++ scripts/mockUsers.js
@@ -0,0 +1,53 @@
+const mockUsers = [
+  {
+    name: "Rick Grimes",
+    userID: "5xXuxzeaOdlQQjwoRSeJ",
+    profileURL:
+      "https://static.wikia.nocookie.net/twd/images/5/53/Season_nine_rick_grimes.png/revision/latest?cb=20201218192153&path-prefix=pt-br",
+  },
+  {
+    name: "Daryl Dixon",
+    userID: "60UVBU4wXmg8KkhM9z0V",
+    profileURL:
+      "https://static.wikia.nocookie.net/twd/images/0/04/Daryl_dixon_season_ten.png/revision/latest?cb=20210224011632&path-prefix=pt-br",
+  },
+  {
+    name: "Negan",
+    userID: "aOJz1CMCLBr2OyiUZxBa",
+    profileURL:
+      "https://static.wikia.nocookie.net/twd/images/7/74/Negan_s10.jpg/revision/latest?cb=20190924221815&path-prefix=pt-br",
+  },
+  {
+    name: "Michonne",
+    userID: "hhL9YXofcYc47gQEjwFQ",
+    profileURL:
+      "https://static.wikia.nocookie.net/twd/images/8/8c/T10Michonne.jpg/revision/latest?cb=20191006190147&path-prefix=pt-br",
+  },
+  {
+    name: "Carol Peletier",
+    userID: "luhCAFf6IZqi483LWRaf",
+    profileURL:
+      "https://static.wikia.nocookie.net/twd/images/5/5f/T10Carol.jpg/revision/latest?cb=20191006190142&path-prefix=pt-br",
+  },
+
+  {
+    name: "Glenn Rhee",
+    userID: "pKi7omxCl3onZnbqKVgr",
+    profileURL:
+      "https://static.wikia.nocookie.net/twd/images/3/32/GlennS7.png/revision/latest/scale-to-width-down/210?cb=20181118191714&path-prefix=pt-br",
+  },
+  {
+    name: "Maggie Greene",
+    userID: "syK2CJnskgEfkT7rrDXw",
+    profileURL:
+      "https://static.wikia.nocookie.net/twd/images/f/f0/Season_nine_maggie_rhee.png/revision/latest?cb=20181113162150&path-prefix=pt-br",
+  },
+  {
+    name: "Carl Grimes",
+    userID: "tyaSlywhgY8I4zmXuBqF",
+    profileURL:
+      "https://static.wikia.nocookie.net/twd/images/7/7b/Carl8.png/revision/latest?cb=20171213125640&path-prefix=pt-br",
+  },
+];
+
+module.exports = { mockUsers };
