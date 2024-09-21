c6fecad6d3fa755ccb6b26fa619090ea9860d88f
Will Gomes <w1llmarcio.dev@hotmail.com>
2024-09-21 17:23:23 -0300

    feat(home-header): check current user before load users

--- api/methods/getUsers.ts
+++ api/methods/getUsers.ts
@@ -1,9 +1,15 @@
+import { where } from "firebase/firestore";
 import { requestAdapter } from "../adapters/requestAdapter";
 import { usersRef } from "../firebase/config";
-import { getUsersMapper } from "../mappers/getUsersMapper";
+import { getUsersMapper } from "../mappers";
+import { CURRENT_USER_ID } from "../utils";
 
 export const getUsers = () => {
-  const usersList = requestAdapter(usersRef, getUsersMapper);
+  const usersList = requestAdapter(
+    usersRef,
+    getUsersMapper,
+    where("userID", "!=", CURRENT_USER_ID),
+  );
 
   return usersList;
 };
